03590e03795285c9c0502df2fd39df67e5d2ee01
occi <935673287@qq.com>
2024-10-05 22:31:44 +0800

    test time spent

--- my-rpc-consumer/src/test/java/com/occi/TestRemoteDynamic.java
+++ my-rpc-consumer/src/test/java/com/occi/TestRemoteDynamic.java
@@ -30,10 +30,11 @@ public class TestRemoteDynamic {
         // 测试执行时间
         // 保存10000个用户
         long start = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100000; i++) {
             Response response = userRemote.saveUser(new User(100011L, "张三", "123456", "2", "2"));
             System.out.println(JSONObject.toJSONString(response));
         }
+        // 测试10万次耗时17秒
         System.out.println("耗时秒数：" + (System.currentTimeMillis() - start) / 1000);
     }
 }
